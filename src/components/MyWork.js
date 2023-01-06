368f7bca44435cbb190154a7462a0f7ec6363db0
Sooraj Mohan <soorajmohan7@gmail.com>
2023-01-06 15:19:58 +0000

    added my works page

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Fade, Slide } from 'react-reveal';
 import '../styles/MyWork.css';
 import Constants from './utils/Constant';
+import QatarTops from '../images/Qatar-tops.PNG';
 
 function MyWork() {
 
@@ -21,7 +22,7 @@ function MyWork() {
         <div className="work_underline"></div>
         <span className="work_dot"></span>
         </Slide>
-
+        <Fade bottom delay={1000}>
         <div className='works-subheader'>
           <h3>
             {Constants.WORK_SUBHEADER_1}
@@ -31,10 +32,19 @@ function MyWork() {
             <strong>{Constants.WORK_SUBHEADER_5}</strong>
           </h3>
         </div>
+        </Fade>
       </div>
       <div className="scroll-animation-work">
             <span>Scroll Down</span>
             <div className="vertical_line-work"></div>
+          </div>
+          <div className='all-works'>
+            <div className='work-left'>
+              <div className='work-left-img'>
+                <img id='qatar' src={QatarTops} alt='qatar-tops'/>
+              </div>
+            </div>
+            <div className='work-right'></div>
           </div>
     </section>
   );
