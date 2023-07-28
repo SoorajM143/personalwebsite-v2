342cd67dbb0b17190c27098484445c5d08ade8b1
Sooraj <soorajmohan@gmail.com>
2023-07-28 20:26:18 +0100

    main page changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
+import { Fade, Slide } from 'react-reveal';
 import MainPageHeader from './MainPageHeader';
 import '../styles/MainPage.css';
+import Constant from './utils/Constant';
+import '../styles/Main.css';
+
 
 function Main() {
   useEffect(() => {
@@ -12,6 +16,18 @@ function Main() {
     <>
       <div className="mainPage">
         <MainPageHeader />
+        <div className='mainPage-content'>
+        <Fade top delay={700}>
+          <h2>{Constant.MAIN_PAGE_CONTENT_HEADRER}</h2>
+          <span className="main_dot"></span>
+          </Fade>
+          <Slide right>
+          <div className="main_underline"></div>
+          </Slide>
+          <div className='mainPage-message'>
+            <span>{Constant.MAIN_PAGE_MESSAGE_1}</span>
+          </div>
+        </div>
       </div>
     </>
   );
